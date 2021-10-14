1a6114fa05098c48acf3a0642ae01de3108abbc0
Gabriel Alonso <gabriel.tupi.alonso@gmail.com>
2021-10-14 13:02:34 -0300

    Estudando Delegates

---
+++ LambdaDelegateLinq/Customer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace LambdaDelegateLinq {
+    class Customer {
+
+        private string _name;
+
+        private static uint _count = 0;
+        private const string _defaultName = "Customer";
+        private const int _nameMaxLength = 30;
+
+        public Customer() : this(NextName(), new Random().Next(18, 61), (Sexes)new Random().Next(0,2), 100.0) { }
+
+        public Customer(string name, int age, Sexes sex, double balance) {
+            Name = name;
+            Age = age;
+            Sex = sex;
+            Balance = balance;
+            _count++;
+        }
+
+        public enum Sexes { Male, Female }
+        public string Name {
+            get => _name;
+            set {
+                _name = value.Length > _nameMaxLength ?
+                    value.Substring(0, _nameMaxLength) :
+                    (value.Length < 2 ? NextName() : value);
+            }
+        }
+        public int Age { get; set; }
+        public Sexes Sex { get; set; }
+        public double Balance { get; set; }
+
+        public bool Highlight { get; set; }
+
+        private static string NextName() {
+            return $"{_defaultName} {_count + 1}";
+        }
+
+        public override string ToString() {
+            return
+                $"{Name,-_nameMaxLength}, " +
+                $"{Age,3} yo, " +
+                $"{Sex.ToString()[0]}, " +
+                $"R${Balance.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ LambdaDelegateLinq/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LambdaDelegateLinq {
+    class Program {
+        static void Main(string[] args) {
+
+            Program p = new Program();
+            List<Customer> customers = new List<Customer>();
+
+            for (int i = 0; i < 10; i++) {
+                customers.Add(new Customer());
+            }
+
+            p.PrintCustomerList(customers);
+            customers.ForEach(c => {
+                if (c.Age <= 30) {
+                    c.Highlight = true;
+                    c.Balance += 210.0;
+                }
+            });
+            p.NewLine();
+            p.PrintCustomerList(customers);
+            customers.RemoveAll(c => c.Age >= 40);
+
+            p.NewLine();
+            customers.ForEach(c => { c.Highlight = false; });
+            p.PrintCustomerList(customers);
+            Action<Customer, double> a = (a, b) => { a.Balance += b; };
+            p.NewLine();
+            foreach (Customer c in customers) a(c, 15.0);
+            customers[2].Name = "Fulano da Silva Nome Comprido Pra Testar o Setter";
+            p.PrintCustomerList(customers);
+        }
+
+        public void NewLine() {
+            Console.WriteLine();
+        }
+
+        public void PrintCustomerList(List<Customer> customers) {
+
+            if (customers != null) {
+                foreach (Customer c in customers) {
+                    if (c.Highlight) Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(c);
+                    Console.ResetColor();
+                }
+            }
+        }
+    }
+}
